d6203e18e39c8187690a4a63374970869612e0b7
Nate Norberg <nate.norberg@quiq.com>
2020-02-05 13:13:22 -0700

    Colors

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#fff`,
+        background_color: `#5c2997`,
+        theme_color: `#5c2997`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
